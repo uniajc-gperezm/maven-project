c0eca29269898a3af2cbc9c00ba566f5e2abca68
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2026-02-17 20:59:36 -0500

    agregando clase persona

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,5 +3,14 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        Persona persona1 = new Persona(123, "Gabriel", "email@email.com");
+
+        String obtenerSaludo = persona1.hablar("Hola como estas?");
+
+        System.out.println(obtenerSaludo);
+        System.out.println("Termino!");
+
     }
 }
---
+++ src/main/java/com/example/Persona.java
@@ -0,0 +1,35 @@
+package com.example;
+
+// Esta es la clase molde para crear personas.
+public class Persona {
+    
+    // atributos que identican a una persona
+    private int cedula;
+    private String nombre;
+    String correo;
+
+    // constructor metodo inicial de la clase se ejecuta inmediantamente creemos un objeto o una instancia de la clase.
+    public Persona() {
+        System.out.println("Aqui se ejecuto el constructor vacio");
+    }
+
+    public Persona(int cedula) {
+        System.out.println("Se ejecuto el contructor con la cedula");
+        this.cedula = cedula;
+    }
+
+    public Persona(int cedula, String nombre, String correo) {
+        System.out.println("Se ejecuto el constructor con todos los parametros");
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    // Aqui creamos los metodos que indican las acciones que puede ejecutar o hacer una persona.
+
+    public String hablar(String saludo) {
+        System.out.println("El metodo hablar se ejecuto");
+        return saludo + ": " + this.nombre;
+    }
+
+}
